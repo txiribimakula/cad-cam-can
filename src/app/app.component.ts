05c796498a1ea93cee8dab2378e5f261e5f151d6
Pablo Tobalina <pablotobalina@gmail.com>
2020-10-10 18:19:46 +0200

    Svg animation test.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from "gsap";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cadcamcan';
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    gsap.to("#test", {duration: 1, x: 100});
+  }
 }
